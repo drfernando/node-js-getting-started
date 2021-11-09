07e19df241ec19049cf6bf3d44a7d5eae8d88491
drfernando <acadrfe@gmail.com>
2021-11-09 16:57:28 -0400

    test post

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/hola', function (req, res) {res.send('[GET]Saludos desde express')})
-  .post('/hola', function (req, res) {res.send('[POST]' + req.body)})
+  .post('/hola', parseMessage)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+/* 
+  VI:  093910
+Nombre del agente: Zona Celular 2002
+Proveedor de Internet: Inter
+Numero de telefono contacto: 0414 1187638
+Ubicación : Cua 
+Municipio Urdaneta
+Falla: cvsc
+Cual es la afectación: No indica tecnología del equipo
+Desde cuando presenta la afectación: desde el 09/11/2021 */
   
+  function parseMessage(req, res) {
+    
+
+    var message = req.body;
+
+    if(message.includes("VI") && message.includes("Proveedor de Internet")
+    && message.includes("Numero de telefono contacto") && message.includes("Proveedor de Internet")
+    && message.includes("Ubicación") && message.includes("Falla")
+    && message.includes("Cual es la afectación") 
+    && message.includes("Desde cuando presenta la afectación")){
+      res.send('Procesado exitosamente' + req.body)
+    }else{
+      res.send('Formato Invalido' + req.body)
+    }
+
+    
+  }
