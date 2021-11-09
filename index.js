5a054ba93b7df64cc289658b2451b632852a1607
drfernando <acadrfe@gmail.com>
2021-11-09 15:45:25 -0400

    test

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-const app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.post('/hola', function (req, res) {
-  res.send('[POST]Saludos desde express');
-});
-app.get('/hola', function (req, res) {
-  res.send('[GET]Saludos desde express');
-});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/hola', function (req, res) {res.send('[GET]Saludos desde express')})
+  .post('/hola', function (req, res) {res.send('[POST]Saludos desde express')})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
