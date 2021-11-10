a66b9ca07d708bb5c56a0d0f1a99d5313bfc1840
drfernando <acadrfe@gmail.com>
2021-11-09 20:41:05 -0400

    .

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
 
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.urlencoded)
+  .use(bodyParser.json)
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
