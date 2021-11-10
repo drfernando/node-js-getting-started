d82e4e45dc4d5b5b38b65c92907a5d22bab2e316
drfernando <acadrfe@gmail.com>
2021-11-10 08:33:10 -0400

    .

--- index.js
+++ index.js
@@ -25,38 +25,31 @@ express()
     && message.includes("Numero de telefono contacto") && message.includes("Ubicación") 
     && message.includes("Falla") && message.includes("Cual es la afectación") 
     && message.includes("Desde cuando presenta la afectación")){
-      var x = "VI:"
-      var startIndex = message.indexOf(x) + x.length()
+      var startIndex = message.indexOf("VI:") + "VI:".length
       var endIndex = message.indexOf("Proveedor de Internet:")
       var VIValue = message.substring(startIndex,endIndex).trim()
       console.log(VIValue)
-      x = "Proveedor de Internet:"
-      startIndex = message.indexOf(x) + x.length()
+      startIndex = message.indexOf("Proveedor de Internet:") + "Proveedor de Internet:".length
       endIndex = message.indexOf("Numero de telefono contacto:")
       var Provider = message.substring(startIndex,endIndex).trim()
       console.log(Provider)
-      x = "Numero de telefono contacto:"
-      startIndex = message.indexOf(x) + x.length()
+      startIndex = message.indexOf("Numero de telefono contacto:") + "Numero de telefono contacto:".length
       endIndex = message.indexOf("Ubicación:")
       var Number = message.substring(startIndex,endIndex).trim()
       console.log(Number)
-      x = "Ubicación:"
-      startIndex = message.indexOf(x) + x.length()
+      startIndex = message.indexOf("Ubicación:") + "Ubicación:".length
       endIndex = message.indexOf("Falla:")
       var Dir = message.substring(startIndex,endIndex).trim()
       console.log(Dir)
-      x = "Falla:"
-      startIndex = message.indexOf(x) + x.length()
+      startIndex = message.indexOf("Falla:") + "Falla:".length
       endIndex = message.indexOf("Cual es la afectación:")
       var Problem = message.substring(startIndex,endIndex).trim()
       console.log(Problem)
-      x = "Cual es la afectación:"
-      startIndex = message.indexOf(x) + x.length()
+      startIndex = message.indexOf("Cual es la afectación:") + "Cual es la afectación:".length
       endIndex = message.indexOf("Desde cuando presenta la afectación:")
       var Descrip = message.substring(startIndex,endIndex).trim()
       console.log(Descrip)
-      x = "Desde cuando presenta la afectación:"      
-      startIndex = message.indexOf(x) + x.length()
+      startIndex = message.indexOf("Desde cuando presenta la afectación:") + "Desde cuando presenta la afectación:".length
       endIndex = message.length()
       var Date = message.substring(startIndex,endIndex).trim()
       console.log(Date)      
